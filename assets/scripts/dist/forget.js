ef898bccd62ee24f2f5b955fe05d10642c6e0f70
Bruno Romeo <brunogfvot@hotmail.com>
2023-04-19 20:52:43 -0300

    Correção no recuperar senha

--- assets/scripts/dist/forget.js
+++ assets/scripts/dist/forget.js
@@ -4,16 +4,17 @@ if (formForget) {
     formForget.addEventListener("submit", (e) => {
         e.preventDefault();
         let userItem = localStorage.getItem('user');
-        let users = JSON.parse(userItem);
+        let users = userItem ? JSON.parse(userItem) : [];
         let emailFind = document.getElementById("email");
         let emailEncontrado = false;
-        const userFind = users.find(user => user.email === emailFind.value);
+        const userFind = users.find((user) => user.email === emailFind.value);
         let span = document.querySelector("#forget-msg");
         if (userFind) {
-            span.innerHTML = "E-mail enviado com sucesso";
+            alert(`Instruções enviada para o email: ${emailFind.value}`);
+            window.location.href = "auth.html";
         }
         else {
-            span.innerHTML = "E-mail não cadastrado";
+            alert("E-mail não cadastrado");
         }
     });
 }
